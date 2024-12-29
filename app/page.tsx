a68b6a8efe343c7051fe3079e36c3e84ce0effa4
POLOWB <wabbaye50paul@gmail.com>
2024-12-29 10:01:44 +0300

    Finish home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { FcPrivacy } from "react-icons/fc";
 import {Poppins} from 'next/font/google'
 import {cn} from '../lib/utils'
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
 
 const font = Poppins({
   subsets:['latin'],
@@ -25,9 +26,12 @@ export default function Home() {
           A simple authentication service
         </p>
         <div>
-          <Button variant='secondary'size='lg'>
-            Sign In
-          </Button>
+          <LoginButton>
+            <Button variant='secondary'size='lg'>
+              Sign In
+            </Button> 
+          </LoginButton>
+        
         </div>
       </div>
     </main>
