73dfcaa2e06b43501c4d856a0d4c46b0c5176466
POLOWB <wabbaye50paul@gmail.com>
2024-12-31 10:11:35 +0300

    Create register form and register actions

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { RegisterForm } from '@/components/auth/register-form copy'
+
+const RegisterPage = () => {
+  return (
+    <RegisterForm />
+  )
+}
+
+export default RegisterPage
